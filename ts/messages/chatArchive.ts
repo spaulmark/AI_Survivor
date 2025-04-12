3aa91e9cb545e769c8c6fdfc52ae2bb91e499a63
Spencer <fdoou1@gmail.com>
2025-04-12 18:56:54 -0400

    Rework chat archive for easier data recovery

--- ts/messages/chatArchive.ts
+++ ts/messages/chatArchive.ts
@@ -3,7 +3,6 @@ export interface Message {
   to: string;
   text: string;
   time: Time;
-  // TODO: maybe an additional field for special JSON symbols?
 }
 
 export interface Time {
@@ -12,20 +11,20 @@ export interface Time {
 }
 
 export interface ChatArchiveInfo {
-  messages: { [key: string]: Message[] };
+  messages: { [name1: string]: { [name2: string]: Message[] } };
   round: number;
   current_message: number;
 }
 
-function getKey(m: Message | string[]): string {
+function getKey(m: Message | string[]): string[] {
   if (Array.isArray(m)) {
-    return m.sort().join("");
+    return m.sort();
   }
-  return [m.to, m.from].sort().join("");
+  return [m.to, m.from].sort();
 }
 
 export class ChatArchive {
-  private messages: { [key: string]: Message[] } = {};
+  private messages: { [name1: string]: { [name2: string]: Message[] } } = {};
 
   private round = 0;
   private current_message = 0;
@@ -46,16 +45,33 @@ export class ChatArchive {
 
   public addMessage(m: Message): void {
     const key = getKey(m);
-    if (key in this.messages) {
-      this.messages[key].push(m);
-    } else {
-      this.messages[key] = [m];
+
+    if (!(key[0] in this.messages)) {
+      this.messages[key[0]] = {};
     }
+    if (!(key[1] in this.messages[key[0]])) {
+      this.messages[key[0]][key[1]] = [m];
+      return;
+    }
+    this.messages[key[0]][key[1]].push(m);
   }
+
+  public getManyChatlogs(
+    hero: string,
+    villains: string[]
+  ): { [name: string]: Message[] } {
+    const result: any = {};
+    for (const villain of villains) {
+      result[villain] = this.getChatlog(hero, villain);
+    }
+    return result;
+  }
+
   public getChatlog(hero: string, villian: string): Message[] {
     const key = getKey([hero, villian]);
-    if (key in this.messages) return this.messages[key];
-    else return [];
+    if (!(key[0] in this.messages)) return [];
+    if (!(key[1] in this.messages[key[0]])) return [];
+    return this.messages[key[0]][key[1]];
   }
 
   public getCurrentTime(): Time {
